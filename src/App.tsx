fa90e4dce6e20f5bb3ea94a1684a477051b4bac7
Dimitar Zahariev <dbzahariev@gmail.com>
2021-04-30 16:17:35 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Typography } from "antd";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography.Text style={{ fontSize: 30 }}>Hello world</Typography.Text>
     </div>
   );
 }
