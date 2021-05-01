403525a73a403b51363c9d5bac1f68ab63050274
Dimitar Zahariev <dbzahariev@gmail.com>
2021-05-01 13:05:46 +0300

    Finished styling the header

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import NumberDisplay from "../NumberDisplay";
 import "./App.scss";
 
 const App: React.FC = () => {
-  return <div className="App">Hello from app!</div>;
+  return (
+    <div className="App">
+      <div className="Header">
+        <NumberDisplay value={0} />
+        <div className="Face">😁</div>
+        <NumberDisplay value={23} />
+      </div>
+      <div className="Body">body</div>
+    </div>
+  );
 };
 
 export default App;
