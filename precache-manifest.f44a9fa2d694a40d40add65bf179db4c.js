af3418fe1ac6cb89bda7ce9e30fe07bb75fc7a11
Dimitar Zahariev <dbzahariev@gmail.com>
2021-05-05 16:19:53 +0300

    Updates

---
+++ precache-manifest.f44a9fa2d694a40d40add65bf179db4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9f8c465154632044355c7cfb8d68dc",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "6d880338d930be53d9e0",
+    "url": "/minesweeper/static/css/main.4fd5d729.chunk.css"
+  },
+  {
+    "revision": "84a42e96a335c437f771",
+    "url": "/minesweeper/static/js/2.b96d15b5.chunk.js"
+  },
+  {
+    "revision": "6d880338d930be53d9e0",
+    "url": "/minesweeper/static/js/main.f7cecca7.chunk.js"
+  },
+  {
+    "revision": "98b8cbca7a190735a772",
+    "url": "/minesweeper/static/js/runtime-main.82c4d401.js"
+  }
+]);
